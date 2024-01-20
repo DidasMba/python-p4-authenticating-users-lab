fa6cac39cf0bce447a23013205321d57363cdf6d
DidasMba <daniel.mbarushi@gmail.com>
2024-01-20 19:33:31 +0300

    sessions

---
+++ server/resources/sessions.py
@@ -0,0 +1,19 @@
+from flask import session, request
+from flask_restful import Resource
+
+from models import User  # Assuming you have a User model
+
+class LoginResource(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        # Retrieve the user by username
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            # Set the session's user_id value to the user's id
+            session['user_id'] = user.id
+            return user.serialize(), 200
+        else:
+            return {'error': 'Invalid username'}, 401
